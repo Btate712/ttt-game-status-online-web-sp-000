42d9a6cb37e0291b52c17e0690ecb770ccf6cd33
Robert Tate <btate712@gmail.com>
2019-03-10 13:59:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,22 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = 
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if (combination[0] == "X" || combination[0] == "O") &&
+      (combination[0] == combination[1] && combination[1] == combination[2])
+      return combination
+    end
+  end
+  return false
+end
